6c670e9d22a530ccd70ca64d4b52761db1d18bfc
Michael.Piotrowski <mlp95747@gmail.com>
2017-10-29 18:34:03 -0700

    Initial commit

---
+++ Callculator/src/testCalc/Callculator.java
@@ -0,0 +1,33 @@
+
+package testCalc;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Michael Lee Piotrowski
+ * Oct. 2017
+ * Basic calculator that will eventually become
+ * a app using Javafx and CSS
+ */
+public class Callculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner calc = new Scanner(System.in);
+        
+            double fnum, snum, answer;
+            
+        System.out.println("What is the first number? ");
+            fnum = calc.nextDouble();
+        System.out.println("What is the second number? ");
+            snum = calc.nextDouble();
+            answer = fnum + snum;
+        System.out.println("The answer is " + answer);
+                    
+        
+    }
+    
+}
